c6f811582fba9b4fa0ac323aa8024e36b45abe81
Danya <torinpigalev@gmai.com>
2023-02-14 12:03:25 +0200

    add "Виправлення"

--- src/hillel/java/pigalev/basic/hw7/Matrix.java
+++ src/hillel/java/pigalev/basic/hw7/Matrix.java
@@ -2,6 +2,7 @@ package hillel.java.pigalev.basic.hw7;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Matrix {
     public static void main(String[] args) {
@@ -10,22 +11,39 @@ public class Matrix {
         int sizeM = sizeMatrix.nextInt();
         System.out.print("Строки: ");
         int sizeN = sizeMatrix.nextInt();
-        int[][] firstMatrix = new int[sizeM][sizeN];
-        int[][] secondMatrix = new int[sizeN][sizeM];
+        int[][] firstMatrix = new int[sizeN][sizeM];
+        int[][] secondMatrix = new int[sizeM][sizeN];
+
+        // Заповненя матриці
+
         for (int i = 0; i < firstMatrix.length; i++) {
-            for (int j = 0; j < firstMatrix[i].length ; j++) {
-                System.out.print(firstMatrix[i][j] + ", ");
+            for (int j = 0; j < firstMatrix[i].length; j++) {
+                firstMatrix[i][j] = ThreadLocalRandom.current().nextInt(0, 10);
             }
-            System.out.println();
         }
 
         System.out.println();
 
-        for (int i = 0; i < secondMatrix.length ; i++) {
-            for (int j = 0; j <secondMatrix[i].length ; j++) {
-                System.out.print(secondMatrix[i][j] + ", ");
+        // вивід матриці
+
+        for (int[] row : firstMatrix) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        System.out.println();
+
+        // Переініцалізація
+
+        for (int i = 0; i < sizeM; i++) {
+            for (int j = 0; j < sizeN; j++) {
+                secondMatrix[i][j] = firstMatrix[j][i];
             }
-            System.out.println();
+        }
+
+        // вивід матриці
+
+        for (int[] row : secondMatrix) {
+            System.out.println(Arrays.toString(row));
         }
     }
 }
