1ee4b4912ef84588435300c7c684a40f2360b4a0
Danya <torinpigalev@gmai.com>
2023-02-27 21:05:00 +0200

    Add "Information"

--- src/hillel/java/pigalev/basic/hw9/FitnesProgram/Information.java
+++ src/hillel/java/pigalev/basic/hw9/FitnesProgram/Information.java
@@ -13,16 +13,19 @@ public class Information {
     private int pressure;
     private String stepsTaken;
     private int age;
-    public Information(String name,
-                       String dayBirthday,
-                       String monthBirthday,
-                       int yearBirthday,
-                       String phoneNumber,
-                       String email,
-                       String lastName,
-                       int weight,
-                       int pressure,
-                       String stepsTaken) {
+
+    public Information(
+            String name,
+            String dayBirthday,
+            String monthBirthday,
+            int yearBirthday,
+            String phoneNumber,
+            String email,
+            String lastName,
+            int weight,
+            int pressure,
+            String stepsTaken
+    ){
         this.name = name;
         this.dayBirthday = dayBirthday;
         this.monthBirthday = monthBirthday;
@@ -35,57 +38,73 @@ public class Information {
         this.stepsTaken = stepsTaken;
         this.age = 2023 - yearBirthday;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getName() {
         return name;
     }
+
     public String getDayBirthday() {
         return dayBirthday;
     }
+
     public String getMonthBirthday() {
         return monthBirthday;
     }
+
     public int getYearBirthday() {
         return yearBirthday;
     }
+
     public int getAge() {
         return age;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public int getWeight() {
         return weight;
     }
+
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
     public int getPressure() {
         return pressure;
     }
+
     public void setPressure(int pressure) {
         this.pressure = pressure;
     }
+
     public String getStepsTaken() {
         return stepsTaken;
     }
+
     public void setStepsTaken(String stepsTaken) {
         this.stepsTaken = stepsTaken;
     }
+
     public void printAccountInfo() {
-        System.out.println("name: " + getName());
-        System.out.println("dateBirthday: " + getDayBirthday() + "." + getMonthBirthday() + "." + getYearBirthday());
-        System.out.println("age: " + getAge());
-        System.out.println("phoneNumber: " + getPhoneNumber());
-        System.out.println("email: " + getEmail());
+        System.out.println("name: " + name);
+        System.out.println("dateBirthday: " + dayBirthday + "." + monthBirthday + "." + yearBirthday);
+        System.out.println("age: " + age);
+        System.out.println("phoneNumber: " + phoneNumber);
+        System.out.println("email: " + email);
         System.out.println("lastName: " + lastName);
         System.out.println("weight: " + weight);
         System.out.println("pressure: " + pressure);
