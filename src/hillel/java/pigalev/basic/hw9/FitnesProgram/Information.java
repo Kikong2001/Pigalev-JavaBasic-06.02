6fd3383e8868d40c9920f536d863c8a4072225f0
Danya <torinpigalev@gmai.com>
2023-02-27 20:49:52 +0200

    Add "Account"

--- src/hillel/java/pigalev/basic/hw9/FitnesProgram/Information.java
+++ src/hillel/java/pigalev/basic/hw9/FitnesProgram/Information.java
@@ -1,18 +1,28 @@
 package hillel.java.pigalev.basic.hw9.FitnesProgram;
 
 public class Information {
+
     private String name;
     private String dayBirthday;
-    private  String monthBirthday;
+    private String monthBirthday;
     private int yearBirthday;
     private String phoneNumber;
     private String email;
-
-    public String lastName;
-    public int weight;
-    public int pressure;
-    public  String stepsTaken;
-    public Information(String name, String dayBirthday, String monthBirthday, int yearBirthday, String phoneNumber, String email, String lastName, int weight, int pressure, String stepsTaken){
+    private String lastName;
+    private int weight;
+    private int pressure;
+    private String stepsTaken;
+    private int age;
+    public Information(String name,
+                       String dayBirthday,
+                       String monthBirthday,
+                       int yearBirthday,
+                       String phoneNumber,
+                       String email,
+                       String lastName,
+                       int weight,
+                       int pressure,
+                       String stepsTaken) {
         this.name = name;
         this.dayBirthday = dayBirthday;
         this.monthBirthday = monthBirthday;
@@ -22,39 +32,55 @@ public class Information {
         this.lastName = lastName;
         this.weight = weight;
         this.pressure = pressure;
-        this.stepsTaken =stepsTaken;
+        this.stepsTaken = stepsTaken;
+        this.age = 2023 - yearBirthday;
     }
-    private  int age ;
-
     public String getEmail() {
         return email;
     }
-
     public String getName() {
         return name;
     }
-
     public String getDayBirthday() {
         return dayBirthday;
     }
-    public String getMonthBirthday(){
+    public String getMonthBirthday() {
         return monthBirthday;
     }
-
     public int getYearBirthday() {
         return yearBirthday;
     }
-
     public int getAge() {
         return age;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
-    public void printAccountInfo(){
-        this.age = 2023 - yearBirthday;
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getPressure() {
+        return pressure;
+    }
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+    public String getStepsTaken() {
+        return stepsTaken;
+    }
+    public void setStepsTaken(String stepsTaken) {
+        this.stepsTaken = stepsTaken;
+    }
+    public void printAccountInfo() {
         System.out.println("name: " + getName());
         System.out.println("dateBirthday: " + getDayBirthday() + "." + getMonthBirthday() + "." + getYearBirthday());
         System.out.println("age: " + getAge());
@@ -66,5 +92,4 @@ public class Information {
         System.out.println("stepsTaken: " + stepsTaken);
         System.out.println("");
     }
-
 }
