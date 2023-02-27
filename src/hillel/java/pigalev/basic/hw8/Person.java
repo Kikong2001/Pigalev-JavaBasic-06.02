760ac1a9e37b1753978f451def7c8b8d83898c63
Danya <torinpigalev@gmai.com>
2023-02-27 21:34:01 +0200

    Add "Person"

--- src/hillel/java/pigalev/basic/hw8/Person.java
+++ src/hillel/java/pigalev/basic/hw8/Person.java
@@ -1,16 +1,21 @@
 package hillel.java.pigalev.basic.hw8;
 
 public class Person {
-    public String personInfo(String name, String lastName, String city, String numberPhone) {
-        return "Зателефонувати громадянину " + name + lastName + "із міста " + city + "можна за номером " + numberPhone;
+
+    public String name;
+    public String lastName;
+    public String city;
+    public String numberPhone;
+
+    public Person(String name, String lastName, String city, String numberPhone) {
+        this.name = name;
+        this.lastName = lastName;
+        this.city = city;
+        this.numberPhone = numberPhone;
     }
-    public static void main(String[] args) {
-        Person personOne = new Person();
-        System.out.println(personOne.personInfo("WillSmith ",  "Smith", " New York ", " 2936729462846"));
-        Person personTwo = new Person();
-        System.out.println(personTwo.personInfo("Jackie ", "Chan", "Shanghai ", "12312412412"));
-        Person personThree = new Person();
-        System.out.println(personThree.personInfo("Sherlock ", "Holmes", "London ", "37742123513"));
+
+    public String personInfo() {
+        return "Зателефонувати громадянину " + name + lastName + "із міста " + city + "можна за номером " + numberPhone;
     }
 }
 
